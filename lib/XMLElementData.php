ae1eb348f1148056efd9ee0524e3135297044dee
Aaron <314152+AaronDDM@users.noreply.github.com>
2022-01-09 22:24:17 -0500

    Updatd the preg replace to exlude numbers. Honestly, this needs to be revisited. (#15)
    Co-authored-by: Aaron de Mello <demello.aaron@gmail.com>

--- lib/XMLElementData.php
+++ lib/XMLElementData.php
@@ -69,7 +69,7 @@ class XMLElementData
      */
     public function setName(string $name): XMLElementData
     {
-        $name = preg_replace('/([^a-zA-Z\-\_\:]+)/', '', $name);
+        $name = preg_replace('/(^[0-9]+|[^a-zA-Z0-9\-\_\:]+)/', '', $name);
 
         $this->name = $name;
         return $this;
